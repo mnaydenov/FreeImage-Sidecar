448e45a346cfb3e5f2682d050e07a97d82695e68
mnaydenov <garfieldhq@yahoo.com>
2021-10-23 14:53:21 +0300

    Add FindLCMS2

---
+++ cmake/FindLCMS2.cmake
@@ -0,0 +1,21 @@
+find_path(
+  LCMS2_INCLUDE_DIR 
+  NAMES lcms2.h
+  DOC "lcms2.h folder"
+  )
+
+find_library(
+  LCMS2_LIBRARY 
+  NAMES lcms2 
+  DOC "lcms2 library folder"
+  )
+
+mark_as_advanced(LCMS2_INCLUDE_DIR LCMS2_LIBRARY)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LCMS2 DEFAULT_MSG LCMS2_LIBRARY LCMS2_INCLUDE_DIR)
+
+if(LCMS2_FOUND)
+  set(LCMS2_INCLUDE_DIRS ${LCMS2_INCLUDE_DIR})
+  set(LCMS2_LIBRARIES ${LCMS2_LIBRARY} )
+endif()
